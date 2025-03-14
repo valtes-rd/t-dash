19658b917fc1afa28b99cfbc6168c759f7520721
naoto.ino <naoto.ino.valtes@gmail.com>
2025-03-14 17:36:30 +0900

    相対パスになるように修正

--- demo/vuejs/components/Sidebar.vue.js
+++ demo/vuejs/components/Sidebar.vue.js
@@ -1,6 +1,7 @@
 Vue.component('Sidebar', {
-  data: function(){ 
+  data: function () {
     return {
+      baseUrl: '/t-dash/demo/vuejs/vue_index.html#/',
       items: [
         // {
         //   icon: 'mdi-home',
@@ -10,104 +11,107 @@ Vue.component('Sidebar', {
         {
           icon: 'mdi-widgets',
           text: 'Combo Box',
-          route:'/Combo_box',
+          route: '/Combo_box',
         },
         {
           icon: 'mdi-widgets',
           text: 'Button',
-          route:'/Button',
+          route: '/Button',
         },
         {
           icon: 'mdi-widgets',
           text: 'Button Group',
-          route:'/BtnGroup',
+          route: '/BtnGroup',
         },
         {
           icon: 'mdi-widgets',
           text: 'Checkbox',
-          route:'/Checkbox',
+          route: '/Checkbox',
         },
         {
           icon: 'mdi-widgets',
           text: 'Floating Action Button',
-          route:'/Floating_action_button',
+          route: '/Floating_action_button',
         },
         {
           icon: 'mdi-widgets',
           text: 'Radio Button',
-          route:'/Radio_button',
+          route: '/Radio_button',
         },
         {
           icon: 'mdi-widgets',
           text: 'Rating',
-          route:'/Rating',
+          route: '/Rating',
         },
         {
           icon: 'mdi-widgets',
           text: 'Select',
-          route:'/Select',
+          route: '/Select',
         },
         {
           icon: 'mdi-widgets',
           text: 'Slider',
-          route:'/Slider',
+          route: '/Slider',
         },
         {
           icon: 'mdi-widgets',
           text: 'Switch',
-          route:'/Switch',
+          route: '/Switch',
         },
         {
           icon: 'mdi-widgets',
           text: 'Text Field',
-          route:'/Text_field',
+          route: '/Text_field',
         },
         {
           icon: 'mdi-widgets',
           text: 'Transfer List',
-          route:'/Transfer_List',
+          route: '/Transfer_List',
         },
         {
           icon: 'mdi-widgets',
           text: 'Toggle Button',
-          route:'/Toggle_Button',
+          route: '/Toggle_Button',
         },
         {
           icon: 'mdi-widgets',
           text: 'List',
-          route:'/List',
+          route: '/List',
         },
         {
           icon: 'mdi-widgets',
           text: 'Table',
-          route:'/Table',
+          route: '/Table',
         },
         {
           icon: 'mdi-widgets',
           text: 'Tooltip',
-          route:'/Tooltip',
+          route: '/Tooltip',
         },
         {
           icon: 'mdi-widgets',
           text: 'Dialogs',
-          route:'/Dialogs',
+          route: '/Dialogs',
         },
         {
           icon: 'mdi-widgets',
           text: 'Accordion',
-          route:'/Accordion',
+          route: '/Accordion',
         },
       ],
-    }
+    };
   },
-  created: function() {},
-  methods: {},
-  template:
-  `<v-navigation-drawer app clipped permanent="true">
+  created: function () {},
+  methods: {
+    getFullRoute: function (item) {
+      return this.baseUrl + item.route;
+    },
+  },
+  template: `<v-navigation-drawer app clipped permanent="true">
       <v-list-item
       v-for="(item, i) in items"
       :key="i"
-      route :to="item.route">
+      route :to="getFullRoute(item)">
         <v-list-item-icon>
           <v-icon v-text="item.icon"></v-icon>
         </v-list-item-icon>
@@ -115,5 +119,5 @@ Vue.component('Sidebar', {
           <v-list-item-title v-text="item.text"></v-list-item-title>
         </v-list-item-content>
       </v-list-item>
-  </v-navigation-drawer>`
-})
+  </v-navigation-drawer>`,
+});
