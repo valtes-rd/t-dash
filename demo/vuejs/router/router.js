6f7afd54093e3e322258fb5e8994d1dc458eddb6
VAL-MITA-KAZUHIRO <b-kazuhiro.mita@valtes.co.jp>
2025-03-17 16:41:12 +0900

    修正

--- demo/vuejs/router/router.js
+++ demo/vuejs/router/router.js
@@ -20,6 +20,6 @@ const router = new VueRouter({
     { path: '/Tooltip', component: Tooltip },
     { path: '/Transfer_list', component: Transfer_List },
   ],
-  mode: 'hash',
+  // mode: 'hash',
   base: '/t-dash/demo/vuejs/vue_index.html#',
 });
